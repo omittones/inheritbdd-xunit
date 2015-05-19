4ed46f629d91f70af93bd38057f14680251c3b25
Augustin Juricic <ajuricic@hrcloud.com>
2015-05-19 11:08:56 +0200

    Added specialized project for debugging test framework.

---
+++ src/BDD.Debugger/Program.cs
@@ -0,0 +1,20 @@
+﻿using BDD.Framework.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Sdk;
+
+namespace BDD.Debugger
+{
+    public static class Program
+    {
+        public static int Main(string[] args)
+        {
+            return BDD.Framework.Program.Main(args);
+        }
+    }
+}
