21b8a93e8d64380b944ecbb4c32c3a4fcdb1459d
Augustin Juricic <ajuricic@neogov.net>
2016-05-20 17:20:22 +0200

    Added test project for NuGet.

---
+++ src/InheritBDD.xUnit.Nuget/Given_nuget.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace InheritBDD.Nuget
+{
+    public class Given_nuget
+    {
+        [Fact]
+        public void Verify_it_works_ok()
+        {
+            Assert.Equal(true, true);
+        }
+    }
+}
---
+++ src/InheritBDD.xUnit.Nuget/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using InheritBDD.xUnit;
+
+namespace InheritBDD.Nuget
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            SelfHostedRunner.RunAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
